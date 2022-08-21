5b4d2b439d5f2a630d478aff7f7717e08ceefed0
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2022-08-21 13:11:55 +0300

    1+

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const storage = localStorage.getItem('videoplayer-current-time');
+
+    player.on(
+        'timeupdate', throttle(function () {
+          player.getCurrentTime()
+            .then(function (seconds) {
+              localStorage.setItem('videoplayer-current-time', seconds);
+            })
+            .catch(function (error) {});
+        }, 1000),
+        player
+          .setCurrentTime(storage)
+          .then(function (seconds) {})
+          .catch(function (error) {})
+      );
